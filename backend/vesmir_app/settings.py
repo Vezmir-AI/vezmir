b40bc9381c6b0e466fa0427e5db6110683b1f00a
Jean-Baptiste Conan <jeanbaptiste.conan.jbc@gmail.com>
2024-06-26 19:47:43 +0200

    django rest framework

--- backend/vesmir_app/settings.py
+++ backend/vesmir_app/settings.py
@@ -25,7 +25,8 @@ SECRET_KEY = "***REMOVED-DJANGO-SECRET-KEY***"
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
+CORS_ORIGIN_ALLOW_ALL = True
 
 
 # Application definition
@@ -47,6 +48,8 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
+    "django.middleware.common.CommonMiddleware",
 ]
 
 ROOT_URLCONF = "vesmir_app.urls"
