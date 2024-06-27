259d39ba5663b243712fcc02c57691af268c6be9
Jean-Baptiste Conan <jeanbaptiste.conan.jbc@gmail.com>
2024-06-27 02:07:05 +0200

    docker config ok

--- backend/vesmir_app/settings.py
+++ backend/vesmir_app/settings.py
@@ -83,7 +83,7 @@ DATABASES = {
         "NAME": "vesmir",
         "USER": "postgres",
         "PASSWORD": "bite",
-        "HOST": "127.0.0.1",
+        "HOST": "db",
         "PORT": "5432",
     }
 }
