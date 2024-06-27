e3e8fe7ac5905c0b74429ce121f8046492456a19
Jean-Baptiste Conan <jeanbaptiste.conan.jbc@gmail.com>
2024-06-27 17:56:50 +0200

    renaming

--- backend/vesmir_app/settings.py
+++ backend/vesmir_app/settings.py
@@ -79,7 +79,7 @@ WSGI_APPLICATION = "vesmir_app.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "ENGINE": "django.db.backends.postgresql",
         "NAME": "vesmir",
         "USER": "postgres",
         "PASSWORD": "bite",
