487aaddd62e88cf7565b3a7195bb7b0d9030c9c2
JAYB <48089220+jbccc@users.noreply.github.com>
2024-07-21 15:49:08 +0200

    adding user auth (#13)

---
+++ backend/users/auth_views.py
@@ -0,0 +1,66 @@
+from django.contrib.auth import authenticate
+from rest_framework import generics, permissions, status
+from rest_framework.authtoken.models import Token
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework_simplejwt.tokens import RefreshToken, AccessToken
+from rest_framework_simplejwt.token_blacklist.models import OutstandingToken
+
+from .serializers import UserSerializer, UserLoginSerializer
+
+
+class UserRegistrationView(APIView):
+    """
+    Creates the user.
+    """
+
+    permission_classes = [permissions.AllowAny]
+    serializer_class = UserSerializer
+
+    def post(self, request):
+
+        if request.user.is_authenticated:
+            return Response(
+                {"error": "Useralready exists"}, status=status.HTTP_400_BAD_REQUEST
+            )
+
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid():
+            user = serializer.save()
+            if user:
+                tokens = RefreshToken.for_user(user)
+                return Response(
+                    {
+                        "refresh": str(tokens),
+                        "access": str(tokens.access_token),
+                    },
+                    status=status.HTTP_201_CREATED,
+                )
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class UserLoginView(APIView):
+    permission_classes = (permissions.AllowAny,)
+    serializer_class = UserLoginSerializer
+
+    def post(self, request):
+        serializer = self.serializer_class(data=request.data)
+        if serializer.is_valid():
+            response = {
+                "success": True,
+                "token": serializer.data["token"],
+            }
+            return Response(response, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+
+class UserLogoutView(APIView):
+    permission_classes = (IsAuthenticated,)
+
+    def post(self, request):
+        refresh_token = request.data["refresh"]
+        refresh_token = RefreshToken(refresh_token)
+        refresh_token.blacklist()
+        return Response(status=status.HTTP_205_RESET_CONTENT)
