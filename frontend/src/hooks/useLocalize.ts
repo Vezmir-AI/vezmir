fbd58a8446a389a74212f556143376e33f431247
toniolasebrai <56965445+toniolasebrai@users.noreply.github.com>
2024-07-24 15:24:14 +0200

    Login Page

---
+++ frontend/src/hooks/useLocalize.ts
@@ -0,0 +1,5 @@
+import englishTranslations from '../localization/languages/Eng';
+
+export default function useLocalize(key: string): string {
+  return englishTranslations[key as keyof typeof englishTranslations] || key;
+}
