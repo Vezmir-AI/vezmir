f25b71133aa2667c4ca7442c8b12bf0b7cb94e50
Jean-Baptiste Conan <jeanbaptiste.conan.jbc@gmail.com>
2024-07-25 00:25:15 +0200

    api working

--- backend/chat/urls.py
+++ backend/chat/urls.py
@@ -4,9 +4,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("init_conversation/", views.init_conversation),
-    path("get_conversation/<int:conversation_id>/", views.get_conversation),
-    path("add_message/<int:conversation_id>/", views.add_message),
-    path("talk/<int:conversation_id>/", views.talk),
-    path("fake_talk/<int:conversation_id>/", views.fake_talk),
+    path("ai_models/", views.AIModelView.as_view()),
+    path("ai_models/providers/", views.AIModelProviderView.as_view()),
+    path("chat/conversations/", views.UserConversationView.as_view()),
+    path("chat/conversations/<uuid:conv_id>/", views.ChatMessageView.as_view()),
 ]
