3224c926da3563341328b11dc9095c9a1806987f
toniolasebrai <56965445+toniolasebrai@users.noreply.github.com>
2024-07-25 23:40:28 +0200

    Sign up Google button

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,11 +3,18 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 import { ThemeProvider } from './UI/Theme/ThemeContext.tsx'
+import { GoogleOAuthProvider } from "@react-oauth/google"
+
+const googleClientId = import.meta.env.VITE_GOOGLE_CLIENT_ID;
+
+console.log('Google Client ID:', googleClientId);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <ThemeProvider>
-      <App />
-    </ThemeProvider>
-  </React.StrictMode>,
+  <GoogleOAuthProvider clientId={googleClientId}>
+    <React.StrictMode>
+      <ThemeProvider>
+        <App />
+      </ThemeProvider>
+    </React.StrictMode>
+  </GoogleOAuthProvider>
 )
