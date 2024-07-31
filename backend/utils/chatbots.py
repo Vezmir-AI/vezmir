6f4bee39e955526a5ce18bbc5a2f19d1e84f8f88
Jean-Baptiste Conan <jeanbaptiste.conan.jbc@gmail.com>
2024-07-31 16:19:31 +0200

    adding anthropic API

--- backend/utils/chatbots.py
+++ backend/utils/chatbots.py
@@ -6,6 +6,8 @@ def get_api_response(model_name: str, model_provider: str, messages: list[dict])
     match model_provider:
         case "OpenAI":
             yield from OpenAIAPI.get_response(formatted_messages, model_name)
+        case "Anthropic":
+            yield from AnthropicAPI.get_response(formatted_messages, model_name)
         case "sample":
             yield from SampleAPI.get_response(formatted_messages, model_name)
 
