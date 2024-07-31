51895631ea2ca5b9e9ce3f4e2738b783f5a194ab
Jean-Baptiste Conan <jeanbaptiste.conan.jbc@gmail.com>
2024-07-31 18:06:09 +0200

    implement token count

--- backend/chat/views.py
+++ backend/chat/views.py
@@ -79,22 +79,21 @@ class ChatMessageView(APIView):
         except ChatConversation.DoesNotExist:
             return Response(status=status.HTTP_404_NOT_FOUND)
 
+        # query model name and its provider
+        model_name = request.data.get("model_name")
+        model = AIModel.objects.get(name=model_name)
+        model_provider = model.provider.name
+
         # creates the user message
-        serializer = ChatMessageSerializer(
+        user_serializer = ChatMessageSerializer(
             data={
                 "user": request.user.id,
                 "chat_conversation": conv_id,
                 "content": request.data.get("content"),
                 "role": "user",
+                "model": model.name,
             }
         )
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-
-        # query model name and its provider
-        model_name = request.data.get("model_name")
-        model = AIModel.objects.get(name=model_name)
-        model_provider = model.provider.name
 
         # get the whole conversation to pass to the model
         # TODO: Use self.get? or a serializer?
@@ -105,9 +104,12 @@ class ChatMessageView(APIView):
             many=True,
         ).data
 
+        token_in = 0
+
         def stream_and_save():
             full_response = ""
-            token_in = token_out = 0
+            global token_in
+            token_out = 0
             for chunk in get_api_response(
                 model_name=model_name,
                 model_provider=model_provider,
@@ -117,6 +119,14 @@ class ChatMessageView(APIView):
                 if content := chunk.content:
                     full_response += content
                     yield content
+                if usage := chunk.usage_metadata:
+                    token_in = usage.get("input_tokens") if not token_in else token_in
+                    token_out = (
+                        usage.get("output_tokens") if not token_out else token_out
+                    )
+                if response := chunk.response_metadata:
+                    # TODO implement response metadata, e.g. stop reason
+                    pass
 
             # Save the complete response to the database
             ai_message_serializer = ChatMessageSerializer(
@@ -126,6 +136,7 @@ class ChatMessageView(APIView):
                     "content": full_response,
                     "role": "assistant",
                     "model": model.name,
+                    "num_tokens": token_out,
                 }
             )
             ai_message_serializer.is_valid(
@@ -133,7 +144,13 @@ class ChatMessageView(APIView):
             )  # TODO: handle API problems, like stop generation (i.e. data.completed=False)
             ai_message_serializer.save()
 
-        response = StreamingHttpResponse(stream_and_save(), content_type="text/event-stream")
+        user_serializer.num_tokens = token_in
+        user_serializer.is_valid(raise_exception=True)
+        user_serializer.save()
+
+        response = StreamingHttpResponse(
+            stream_and_save(), content_type="text/event-stream"
+        )
         # do not remove this, it is used by nginx to stream the response
         # https://discovergen.ai/article/creating-a-streaming-chat-application-with-django/
         response["X-Accel-Buffering"] = "no"
