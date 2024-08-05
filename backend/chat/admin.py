0670aaf8dc409f6a76edf01527531ced34de7e2b
Jean-Baptiste Conan <jeanbaptiste.conan.jbc@gmail.com>
2024-08-05 12:05:59 +0200

    adds chatmessage to admin views

--- backend/chat/admin.py
+++ backend/chat/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
-from .models import AIModel, AIModelProvider, ChatConversation
+from .models import AIModel, AIModelProvider, ChatConversation, ChatMessage
 
 admin.site.register(AIModel)
 admin.site.register(AIModelProvider)
 admin.site.register(ChatConversation)
+admin.site.register(ChatMessage)
