934ea5dbe11019a5f1b1340e86639407776ab2fa
toniolasebrai <56965445+toniolasebrai@users.noreply.github.com>
2024-08-15 14:55:00 -0400

    Stripe demo working

--- backend/users/models.py
+++ backend/users/models.py
@@ -46,6 +46,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     preferred_model = models.ForeignKey(
         AIModel, on_delete=models.SET_NULL, null=True, blank=True
     )
+    stripe_customer_id = models.CharField(max_length=50, blank=True, null=True)
 
     stripe_setup_intent = models.CharField(max_length=50, blank=True, null=True)
 
