e4cceb6f37ff3f0b42d7284d105710fdd8539784
toniolasebrai <56965445+toniolasebrai@users.noreply.github.com>
2024-08-15 14:55:00 -0400

    Stripe demo working

--- backend/users/models.py
+++ backend/users/models.py
@@ -51,8 +51,6 @@ class User(AbstractBaseUser, PermissionsMixin):
 
     stripe_setup_intent = models.CharField(max_length=50, blank=True, null=True)
 
-    stripe_setup_intent = models.CharField(max_length=50, blank=True, null=True)
-
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = []
 
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -14,6 +14,7 @@ import StripePayment from './components/StripePayment';
 import './index.css'
 import StripePayment from './components/StripePayment';
 import './index.css'
+import StripePayment from './components/StripePayment';
 
 function App() {
   return (
