00841be3fa15e10ab912e8bab02725648f52e364
Jean-Baptiste Conan <jeanbaptiste.conan.jbc@gmail.com>
2024-08-19 23:21:00 +0200

    loading component direct in tabs

--- frontend/src/components/Dashboard/UsageTab/LoadingComponent.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const LoadingComponent: React.FC = () => {
-    return (
-        <div className="w-full h-full bg-gradient-to-r from-gray-800 via-gray-700 to-gray-800 animate-gradient-loading" />
-    );
-};
-
-export default LoadingComponent;
