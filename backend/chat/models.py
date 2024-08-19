ef5b33fedec8af6da7cef8536f111e651df69108
Jean-Baptiste Conan <jeanbaptiste.conan.jbc@gmail.com>
2024-08-19 22:18:27 +0200

    include generic model name for each provider

--- backend/chat/models.py
+++ backend/chat/models.py
@@ -5,6 +5,7 @@ from django.conf import settings
 
 class AIModelProvider(models.Model):
     name = models.CharField(max_length=100, primary_key=True)
+    display_name = models.CharField(max_length=100)
 
 
 class AIModel(models.Model):
