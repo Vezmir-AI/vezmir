1c187bd3fd254eb6a53e76ef51728210f6b6066d
Jean-Baptiste Conan <jeanbaptiste.conan.jbc@gmail.com>
2024-08-19 23:37:48 +0200

    import alias for src folder

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite'
+import tsconfigPaths from 'vite-tsconfig-paths';
 import react from '@vitejs/plugin-react'
 import tailwindcss from 'tailwindcss'
 import autoprefixer from 'autoprefixer'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tsconfigPaths()],
   css: {
     postcss: {
       plugins: [
