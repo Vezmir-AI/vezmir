78c13fa4a61626ff7a1ecf39293a7148b5af2774
Jean-Baptiste Conan <jeanbaptiste.conan.jbc@gmail.com>
2024-08-19 22:31:32 +0200

    adding UsageTab and WIP billing

--- frontend/src/components/Dashboard.tsx
+++ frontend/src/components/Dashboard.tsx
@@ -1,15 +1,23 @@
 import React from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
 import ProfileTab from "./Dashboard/ProfileTab";
+import UsageTab from './Dashboard/UsageTab';
+import BillingTab from './Dashboard/BillingTab';
 
 const Dashboard: React.FC = () => {
-  const { section } = useParams<{section: string}>();
+  const { section } = useParams<{ section: string }>();
   const navigate = useNavigate();
   let Component: React.FC = () => <div> No tab {section} exists. Redirecting...</div>
-  switch (section){
-   case "profile":
-    Component = ProfileTab;
-    break;
+  switch (section) {
+    case "profile":
+      Component = ProfileTab;
+      break;
+    case "usage":
+      Component = UsageTab;
+      break;
+    case "billing":
+      Component = BillingTab;
+      break;
     default:
       console.error("No tab exists")
       setTimeout(() => {
