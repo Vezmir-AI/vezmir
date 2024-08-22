905e6276a4fb9943d9b2bc323782f592ebc80680
Jean-Baptiste Conan <jeanbaptiste.conan.jbc@gmail.com>
2024-08-22 14:21:32 +0200

    get card issuer logo

---
+++ frontend/src/utils/cardIssuerUtils.ts
@@ -0,0 +1,15 @@
+import VisaLogo from '@/assets/visa.svg';
+import MastercardLogo from '@/assets/mastercard.svg';
+import CreditCardLogo from '@/assets/credit-card.svg';
+
+export const getIssuerLogo = (issuer: string): string => {
+    switch (issuer.toLowerCase()) {
+      case 'visa':
+        return VisaLogo;
+      case 'mastercard':
+        return MastercardLogo;
+      default:
+        return CreditCardLogo;
+    }
+  };
+  
