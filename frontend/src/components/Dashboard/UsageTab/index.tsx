b1ff7f3ead3bc144f6abe42bfdbbbd1db0d01c90
toniolasebrai <56965445+toniolasebrai@users.noreply.github.com>
2024-09-05 19:08:34 -0700

    Dashboard UX

--- frontend/src/components/Dashboard/UsageTab/index.tsx
+++ frontend/src/components/Dashboard/UsageTab/index.tsx
@@ -7,7 +7,7 @@ const UsageTab: React.FC = () => {
   return (
     <div className="xl:px-16 bg-[var(--gray-800)] h-screen">
       <div className="max-w-7xl px-4 mt-12 sm:px-6 lg:px-8">
-        <h1 className="text-2xl px-2 font-semibold leading-7 text-white mb-4">Usage</h1>
+        <h1 className="text-2xl px-2 font-semibold leading-7 text-white mb-4">Usage Information</h1>
         <p className="mt-1 px-2 text-sm leading-6 text-gray-400 mb-6">Here you will find all the information related to your usage</p>
 
         <div className="grid grid-cols-1 lg:grid-cols-2 gap-8">
