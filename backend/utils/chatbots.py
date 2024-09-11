e9b9a180d72b195f54173d529191538ea08d9411
toniolasebrai <56965445+toniolasebrai@users.noreply.github.com>
2024-09-10 18:09:00 -0700

    Yo c'est moi perplexity

--- backend/utils/chatbots.py
+++ backend/utils/chatbots.py
@@ -1,6 +1,6 @@
 import random
 
-from .lib import AnthropicAPI, GoogleAPI, GroqAPI, OpenAIAPI
+from .lib import AnthropicAPI, GoogleAPI, GroqAPI, OpenAIAPI, PerplexityAPI
 
 
 def get_api_response(model_name: str, model_provider: str, messages: list[dict]):
@@ -12,6 +12,8 @@ def get_api_response(model_name: str, model_provider: str, messages: list[dict])
             yield from AnthropicAPI.get_response(formatted_messages, model_name)
         case "Google":
             yield from GoogleAPI.get_response(formatted_messages, model_name)
+        case "Perplexity":
+            yield from PerplexityAPI.get_response(formatted_messages, model_name)
 
 
 def generate_title(user_message):
