37497d1ac09e992b28eae24c0a892ac285b0d9d2
Jean-Baptiste Conan <jeanbaptiste.conan.jbc@gmail.com>
2024-09-14 19:36:43 +0200

    add parent property in the last migration

--- backend/chat/migrations/0002_initial.py
+++ backend/chat/migrations/0002_initial.py
@@ -32,11 +32,6 @@ class Migration(migrations.Migration):
         ),
         migrations.AddField(
             model_name='chatmessage',
-            name='parent',
-            field=models.ForeignKey(null=True, on_delete=django.db.models.deletion.SET_NULL, related_name='children', to='chat.chatmessage'),
-        ),
-        migrations.AddField(
-            model_name='chatmessage',
             name='user',
             field=models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, related_name='messages', to=settings.AUTH_USER_MODEL),
         ),
